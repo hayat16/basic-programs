55c72a29370215317352dae91ad7a91e270713aa
hayat16 <35870219+hayat16@users.noreply.github.com>
2018-02-16 10:34:47 +0600

    Create diagonal sum of matrix.c

---
+++ diagonal sum of matrix.c
@@ -0,0 +1,38 @@
+//square matrix n*n
+#include<stdio.h>
+int main()
+{
+     int a[100][100],sum=0,i,n,j;
+
+    scanf("%d",&n);
+    printf("Enter matrix:\n");
+    for(i=0; i<n; i++)
+    {
+        for(j=0; j<n; j++)
+        {
+
+            scanf("%d",&a[i][j]);
+        }
+
+    }
+
+     for(i=0; i<n; i++)
+    {
+        for(j=0; j<n; j++)
+        {
+            if(i==j)
+           sum=sum+a[i][j];
+        }
+
+    }
+
+       printf("%d\n",sum);
+
+ return 0;
+
+    }
+
+
+
+
+
