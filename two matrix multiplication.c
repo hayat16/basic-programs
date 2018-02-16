81166abfa17c782fcef6d4eddceabde00ee3f4e4
hayat16 <35870219+hayat16@users.noreply.github.com>
2018-02-16 10:23:32 +0600

    Create two matrix multiplication.c

---
+++ two matrix multiplication.c
@@ -0,0 +1,50 @@
+//square matrix n*n
+#include<stdio.h>
+int main()
+{
+     int a[100][100],b[100][100],sum[100][100];
+    int i,j,n;
+    scanf("%d",&n);
+    printf("first matrix:\n");
+    for(i=0; i<n; i++)
+    {
+        for(j=0; j<n; j++)
+        {
+
+            scanf("%d",&a[i][j]);
+        }
+
+    }
+     printf("2nd matrix\n");
+     for(i=0; i<n; i++)
+    {
+        for(j=0; j<n; j++)
+        {
+
+            scanf("%d",&b[i][j]);
+        }
+
+    }
+
+     for(i=0; i<n; i++)
+    {
+        for(j=0; j<n; j++)
+        {
+           sum[i][j]=a[i][j]*b[i][j];
+        }
+
+    }
+     printf(" matrix mul\n");
+     for(i=0; i<n; i++)
+    {
+        for(j=0; j<n; j++)
+        {
+            printf("%d\t",sum[i][j]);
+        }
+        printf("\n");
+
+    }
+    return 0;
+
+}
+
