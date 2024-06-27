1a5d032fad498688e40b9fa1f0f4c7c175513414
Mysha Sarin <35870219+hayat16@users.noreply.github.com>
2024-06-27 22:20:21 +0600

    Create 2D vector-1.cpp

---
+++ 2D vector-1.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main()
+{
+    vector<vector<string> >result;
+    vector<string> row1;
+    vector<string> row2;
+    string s;
+    int n=2;
+    for(int i=0; i<n; i++)
+    {
+        cin>>s;
+        row1.push_back(s);
+    }
+    for(int i=0; i<n; i++)
+    {
+        cin>>s;
+        row2.push_back(s);
+    }
+
+    result.push_back(row1);
+    result.push_back(row2);
+
+    // Accessing and printing elements
+    cout << "Element at result[0][1]: " << result[0][1] << endl; 
+  
+    // Adding a new row
+    vector<string> newRow;
+    for(int i=0; i<n; i++)
+    {
+        cin>>s;
+        newRow.push_back(s);
+    }
+    result.push_back(newRow);
+
+    cout << "Contents of result:" << endl;
+    for (int i = 0; i < result.size(); i++)
+    {
+        for (int j = 0; j < result[i].size(); j++)
+        {
+            cout << result[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
